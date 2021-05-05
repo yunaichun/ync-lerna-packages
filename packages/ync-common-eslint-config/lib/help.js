c1e6839925e8c30087830ed9488db59c7a62cfdb
yunaichun <yunaichun@qingting.fm>
2021-05-05 22:00:10 +0800

    feat: add files config

---
+++ packages/ync-common-eslint-config/lib/help.js
@@ -0,0 +1,17 @@
+const commander = require('commander');
+const chalk = require('chalk');
+
+const packageJson = require('../package.json');
+
+const { log } = console;
+
+function initHelp() {
+  commander
+    .version(packageJson.version)
+    .on('--help', () => {
+      log(chalk.green('run ync-react-cli to init the project.'));
+    })
+    .parse(process.argv);
+}
+
+module.exports = initHelp;
