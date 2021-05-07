4a32e818c0556a13a72bb8de7199552d4d0dffc1
yunaichun <yunaichun@qingting.fm>
2021-05-07 13:09:23 +0800

    feat(NoTicket): update commit check version

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,31 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'type-empty': [2, 'always'],
+    'type-enum': [0],
+    'type-empty': [0],
+    'subject-empty': [0],
+    'scope-case': [0],
+    'commit-check': [2, 'always'],
   },
   plugins: [
     {
       rules: {
-        'type-empty': (options) => {
-          console.log(123, options)
-          // const emptySubject = !subject || subject.trim().length === 0;
-          // if (emptySubject) return [false, `提交 type 不允许为空`];
-          // return [true];
-          return [false, `提交 type 不允许为空`];
+        'commit-check': ({ type, scope, subject }) => {
+          const tips = '\n提交规范<type>(<scope>): <subject>'
+          const types = ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'];
+
+          // == 验证 subject
+          const emptySubject = !subject || subject.trim().length === 0;
+          if (emptySubject) return [false, `subject 不允许为空${tips}`];
+
+          // == 验证 type
+          const illegalType = types.indexOf(type) < 0
+          if (illegalType) return [false, `type 请填写: ${types.join(', ')}${tips}`];
+
+          // == 验证 scope 
+          const scopePattern = /release|NoTicket|[A-Z]+-\d+/;
+          if (!scopePattern.test(scope)) return [false, `scope 请填写: jiraID, NoTicket, release${tips}`];
+          return [true];
         },
       },
     },
